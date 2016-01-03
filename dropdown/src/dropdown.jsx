3e0aa38595a9e336fece4480cb9ad27f6eb56fcf
uri.nativ <uri.nativ@klarna.com>
2016-01-03 18:35:17 +0200

    Lecture 21 Exploring State and Events - Toggling Visibility

--- dropdown/src/dropdown.jsx
+++ dropdown/src/dropdown.jsx
@@ -4,7 +4,10 @@ var ListItem = require('./list-item');
 
 module.exports = React.createClass({
   handleClick: function() {
-    alert('Hello from dropdown');
+    this.setState({open: !this.state.open})
+  },
+  getInitialState: function() {
+    return { open: false }
   },
   render: function() {
     var list = this.props.items.map(function(item) {
@@ -17,7 +20,7 @@ module.exports = React.createClass({
         title={this.props.title} 
         subTitleClassName="caret" 
       />
-      <ul>
+      <ul className={"dropdown-menu " + (this.state.open ? "show" : "") }>
         {list} 
       </ul>
     </div>
