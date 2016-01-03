e6c5956145c59b89ca4ae1b06d799a331e43398e
uri.nativ <uri.nativ@klarna.com>
2016-01-03 18:21:06 +0200

    Lecture 20 Exploring State and Events - Adding Content

--- dropdown/src/dropdown.jsx
+++ dropdown/src/dropdown.jsx
@@ -1,12 +1,15 @@
 var React = require('react');
 var Button = require('./button');
-//var List = require('./list');
+var ListItem = require('./list-item');
 
 module.exports = React.createClass({
   handleClick: function() {
     alert('Hello from dropdown');
   },
   render: function() {
+    var list = this.props.items.map(function(item) {
+      return <ListItem item={item} />
+    });
     return <div className="dropdown">
       <Button
         whenClicked={this.handleClick} 
@@ -14,6 +17,9 @@ module.exports = React.createClass({
         title={this.props.title} 
         subTitleClassName="caret" 
       />
+      <ul>
+        {list} 
+      </ul>
     </div>
   }
 });
